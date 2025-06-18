6e2b7fc429375aefeadc2454a6812a4e8f61a5b5
wrldworry <wrldworry@gmail.com>
2025-06-18 16:25:40 +0300

    Enable GitHub Pages deploy

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
   return {
+    base: '/goit-advancedjs-hw-04/', 
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
